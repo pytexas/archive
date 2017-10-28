f8a1ba1f2aa0a2941c164fda719dcd4ca44780c6
Paul Bailey <paul.m.bailey@gmail.com>
2017-10-28 18:17:38 +0000

    fixed external links

--- fix_links.py
+++ fix_links.py
@@ -5,6 +5,13 @@ import sys
 
 # find 2013 -name "*.html" -print0 | xargs -0 ./fix_links.py
 
+def replace_external (mobj):
+  url = mobj.group(2)
+  if 'pytexas.org' in url:
+    url = '/2013/'
+    
+  return '<a {}="{}"{}>'.format(mobj.group(1), url, mobj.group(3))
+  
 def run (fps):
   for fp in fps:
     with open(fp, 'r') as fh:
@@ -12,9 +19,10 @@ def run (fps):
       
     # html = re.sub('[\./]+/static/', '/2013/static/', html)
     # html = re.sub('[\./]+/media/', '/2013/media/', html)
+    # new_favi = '<link rel="shortcut icon" href="/favicon.ico">'
+    # html = re.sub('<.*?shortcut icon.*?>', new_favi, html)
     
-    new_favi = '<link rel="shortcut icon" href="/favicon.ico">'
-    html = re.sub('<.*?shortcut icon.*?>', new_favi, html)
+    html = re.sub('<a (.*?)=".*?external\.html\?link=(.*?)"(.*?)>', replace_external, html)
     
     with open(fp, 'w') as fh:
       fh.write(html)
