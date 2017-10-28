27ed742f55a372c2c9108b094acfbb393fa452db
Paul Bailey <paul.m.bailey@gmail.com>
2017-10-28 19:16:29 +0000

    fixed external links

--- fix_links.py
+++ fix_links.py
@@ -8,7 +8,10 @@ import sys
 def replace_external (mobj):
   url = mobj.group(1)
   if 'pytexas.org' in url:
-    url = url.replace('http://2013.pytexas.org/media/', '/2013/media/')
+    url = url.replace('http://archive.pytexas.org', '/2014')
+    
+  else:
+    pass
     
   return '="{}"'.format(url)
   
@@ -17,12 +20,12 @@ def run (fps):
     with open(fp, 'r') as fh:
       html = fh.read()
       
-    html = re.sub('[\./]+/static/', '/2014/static/', html)
-    html = re.sub('[\./]+/media/', '/2014/media/', html)
-    new_favi = '<link rel="shortcut icon" href="/favicon.ico">'
-    html = re.sub('<.*?shortcut icon.*?>', new_favi, html)
+    # html = re.sub('[\./]+/static/', '/2014/static/', html)
+    # html = re.sub('[\./]+/media/', '/2014/media/', html)
+    # new_favi = '<link rel="shortcut icon" href="/favicon.ico">'
+    # html = re.sub('<.*?shortcut icon.*?>', new_favi, html)
     
-    # html = re.sub('=".*?external\.html\?link=(.*?)"', replace_external, html)
+    html = re.sub('=".*?external\.html\?link=(.*?)"', replace_external, html)
     
     with open(fp, 'w') as fh:
       fh.write(html)
