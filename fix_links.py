ae3140841bd6c43f2ec6154abf0a67a12a01733e
Paul Bailey <paul.m.bailey@gmail.com>
2017-10-28 19:05:34 +0000

    2014

--- fix_links.py
+++ fix_links.py
@@ -3,7 +3,7 @@
 import re
 import sys
 
-# find 2013 -name "*.html" -print0 | xargs -0 ./fix_links.py
+# find 2014 -name "*.html" -print0 | xargs -0 ./fix_links.py
 
 def replace_external (mobj):
   url = mobj.group(1)
@@ -17,12 +17,12 @@ def run (fps):
     with open(fp, 'r') as fh:
       html = fh.read()
       
-    # html = re.sub('[\./]+/static/', '/2013/static/', html)
-    # html = re.sub('[\./]+/media/', '/2013/media/', html)
-    # new_favi = '<link rel="shortcut icon" href="/favicon.ico">'
-    # html = re.sub('<.*?shortcut icon.*?>', new_favi, html)
+    html = re.sub('[\./]+/static/', '/2014/static/', html)
+    html = re.sub('[\./]+/media/', '/2014/media/', html)
+    new_favi = '<link rel="shortcut icon" href="/favicon.ico">'
+    html = re.sub('<.*?shortcut icon.*?>', new_favi, html)
     
-    html = re.sub('=".*?external\.html\?link=(.*?)"', replace_external, html)
+    # html = re.sub('=".*?external\.html\?link=(.*?)"', replace_external, html)
     
     with open(fp, 'w') as fh:
       fh.write(html)
