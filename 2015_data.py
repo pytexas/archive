c7def29921c6812050f3f1a1da9567b2f8ce21c1
Paul Bailey <paul.m.bailey@gmail.com>
2017-10-28 23:01:23 +0000

    add talks

--- 2015_data.py
+++ 2015_data.py
@@ -18,21 +18,21 @@ def run ():
     with open('2015/api/v1/users/profile/' + d['fields']['username'], 'wb') as fh:
       fh.write(response.content)
       
-    subprocess.call("ln -s ../index.html 2015/user/{}.html".format(d['fields']['username']), shell=True)
+    subprocess.call("ln -sf ../index.html 2015/user/{}.html".format(d['fields']['username']), shell=True)
     
   with open('../talks.json', 'r') as fh:
     data = json.load(fh)
     
   for d in data:
-    print('Talk:', d['fields']['id'])
-    url = 'https://www.pytexas.org/api/v1/users/profile/{}?conf=2015&format=json'.format(d['fields']['username'])
+    print('Talk:', d['pk'])
+    url = 'https://www.pytexas.org/api/v1/speakers/talk/{}?conf=2015&format=json'.format(d['pk'])
     
     response = requests.get(url)
     
-    with open('2015/api/v1/users/profile/' + d['fields']['username'], 'wb') as fh:
+    with open('2015/api/v1/speakers/talk/' + str(d['pk']), 'wb') as fh:
       fh.write(response.content)
       
-    subprocess.call("ln -s ../index.html 2015/user/{}.html".format(d['fields']['username']), shell=True)
+    subprocess.call("ln -sf ../index.html 2015/talk/{}.html".format(d['pk']), shell=True)
     
 if __name__ == '__main__':
   run()
